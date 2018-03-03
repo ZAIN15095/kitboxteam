a0551abc05cc4de20dfdcad5bc64247e1d2bad2d
ZAIN15095 <33008039+ZAIN15095@users.noreply.github.com>
2018-03-03 13:10:04 +0100

    UPDATE

---
+++ Interface/WindowsFormsApp4/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Main : MetroFramework.Forms.MetroForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+            this.StyleManager= msnMain;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
